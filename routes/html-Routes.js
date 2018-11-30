340aebfe3d608ad167fb0e23adf0bf43f1df976d
angeljiang0927@gmail.com <angeljiang0927@gmail.com>
2018-11-29 20:45:12 -0800

    added route for mainpage

--- routes/html-Routes.js
+++ routes/html-Routes.js
@@ -2,7 +2,11 @@ var path = require("path");
 
 module.exports = function(app) {
   // Load index page
+  app.get("/mainPage", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/mainPage.html"));
+  });
+
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-}
+};
